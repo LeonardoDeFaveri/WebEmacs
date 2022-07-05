dfb73f4aa2d9b0b6bbac2b32ceb82a47c2eb4856
LeonardoDeFaveri <leonardo.defaveri01@gmail.com>
2022-07-05 16:11:02 +0200

    Created minimal login form

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -7,8 +7,23 @@ class LoginPage extends React.Component {
 
     render(): React.ReactNode {
         return (
-            <div>
+            <div className="center">
                 <h3>Login page</h3>
+                <form method="post">
+                    <fieldset>
+                        <legend>Login</legend>
+                        <label >Username/email
+                            <input type="text" id="username" name="username" placeholder="username/email" required></input>
+                        </label>
+                        <label>Password
+                            <input type="password" id="password" name="password"></input>
+                        </label>
+                        <div className="row">
+                            <button type="button">Sign Up</button>
+                            <button type="submit">Login</button>
+                        </div>
+                    </fieldset>
+                </form>
             </div>
         )
     }
